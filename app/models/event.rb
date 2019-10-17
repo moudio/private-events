90258bd8a976b3c92417aaa8f2ff4bc5d72134d3
Redvan <redvanisation@gmail.com>
2019-10-17 12:45:08 -0400

    Refactor class methods to scopes in the Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,24 +3,27 @@ class Event < ApplicationRecord
     has_many :user_events, foreign_key: :attended_event_id
     has_many :attendees, through: :user_events, foreign_key: :attendee_id
 
+    scope :upcoming, -> { where("date >= ?", Time.zone.now) }
+    scope :past, -> { where("date < ?", Time.zone.now) }
+
 
     
-     def Event.upcoming
-        upcoming = []
-        arr = self.order(created_at: :desc)
-        arr.each do |event|
-            upcoming << event if event.date && event.date >= Time.zone.now
-        end
-        upcoming
-     end
+    #  def Event.upcoming
+    #     upcoming = []
+    #     arr = self.order(created_at: :desc)
+    #     arr.each do |event|
+    #         upcoming << event if event.date && event.date >= Time.zone.now
+    #     end
+    #     upcoming
+    #  end
 
-     def Event.past
-        past = []
-        arr = self.order(created_at: :desc)
-        arr.each do |event|
-            past << event if event.date && event.date < Time.zone.now
-        end
-        past
-     end
+    #  def Event.past
+    #     past = []
+    #     arr = self.order(created_at: :desc)
+    #     arr.each do |event|
+    #         past << event if event.date && event.date < Time.zone.now
+    #     end
+    #     past
+    #  end
 
 end
